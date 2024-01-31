f615e28fc1fd6457fc7986861742a0e3eb7f7e30
wimdenherder <wimdenherder@gmail.com>
2024-01-31 19:35:46 +0100

    small improvement

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Selecteer alle afbeeldingen met de opgegeven query
 const images = document.querySelector("#main > div:nth-child(4) > div > div:nth-child(3)").querySelectorAll("img");
+const toBeClicked = [...images].filter(x => x.src.indexOf("blob:") !== 0).filter(x => x.parentNode.parentNode.parentNode.innerText.indexOf("www.") === -1);
 
 // Klik op elke afbeelding
-images.forEach(img => {
+toBeClicked.forEach(img => {
     img.click();
 });
 
 // Wacht 5 seconden (5000 milliseconden), en download dan de afbeeldingen
 setTimeout(() => {
-    images.forEach((img, index) => {
+  photos.forEach((img, index) => {
         // Maak een tijdelijke a-element
         const link = document.createElement('a');
         link.href = img.src;
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 # scrape whatsapp conversations
 
 it does not work yet. 
-- click only on images that are not links
-- should wait for images to be loaded
+- click only on images that are not links (for ex youtube links)
+- should wait for images to be loaded (how to detect? thumbnail image disappear and new big image shows up)
 - scrape text as well
 - in this text refer to image name (so that later you can insert the images back there)
+- click and download video's
+- grouped photos should work too
